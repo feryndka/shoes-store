17fedf4b45645c9824c31f4a1f614ec107a03e14
feryndka <feryandika151@gmail.com>
2024-07-09 20:51:38 +0700

    feat:login

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,8 +1,10 @@
+import LoginView from "@/components/views/auth/Login";
+
 const LoginPage = () => {
   return (
-    <div>
-      <div>Login page</div>
-    </div>
+    <>
+      <LoginView />
+    </>
   );
 };
 
